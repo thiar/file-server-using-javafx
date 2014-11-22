4e45345a028f339bd210af4309958124f7e6f5f8
hafiznuzal <hafiz_nuzal@yahoo.co.id>
2014-11-23 06:31:43 +0700

    Update handler.java

--- Server/handler.java
+++ Server/handler.java
@@ -7,22 +7,20 @@
 package server;
 
 import cmd.cmd;
-import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.sql.Time;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -37,9 +35,13 @@ public class handler implements Runnable{
     private ObjectOutputStream objectoutput;
     private ObjectInputStream objectinput;
  
+    private DateFormat dateFormat;
+    private Date date;
+    private String LoginTime;
     private String addr;
     private final ArrayList<handler> alhandler;
     private ArrayList<String> aluser;
+    private int speed=1024;
     
     public handler(Socket cli, ArrayList<handler> alhandler) throws IOException
     {
@@ -48,6 +50,7 @@ public class handler implements Runnable{
         this.objectoutput=new ObjectOutputStream(cli.getOutputStream());
         this.objectinput =new ObjectInputStream(cli.getInputStream());
         
+        
         aluser =new ArrayList<>();
         
      
@@ -55,29 +58,50 @@ public class handler implements Runnable{
     
     public synchronized void ListUser() throws IOException
     {
-        this.aluser.clear();
+        tArrayList<cmd> alluser = new ArrayList<>();
+        
+        
         for(int i=0;i<this.alhandler.size();i++)
         {
+            cmd user=new cmd();
             handler client =this.alhandler.get(i);
+            user.setNamaUser(client.addr);
+            user.setIp(client.sockcli.getInetAddress().toString() +":" + client.sockcli.getPort());
+            user.setLoginDate(client.LoginTime);
+            alluser.add(user);
             System.out.println(client.addr);
-            aluser.add(client.addr);
+            
         }
         
-        this.sendobject(aluser);
+        this.sendobject(alluser);
         
     }
     public void ListFile() throws IOException
     {
-        File[] files = new File("cache\\" + this.addr).listFiles();
+       File[] files = new File("cache\\" + this.addr).listFiles();
         List<String> results = new ArrayList<>();
+        ArrayList<cmd> allfile = new ArrayList<>();
+        
         for (File file : files) {
             if (file.isFile()) {
-                results.add(file.getName());
+                //results.add(file.getName());
+                cmd thefile=new cmd();
+                thefile.setNameFile(file.getName());
+                System.out.println(file.getName());
+                thefile.setFileSize(readableFileSize(file.length()));
+                thefile.setLastModified(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(file.lastModified()));
+                allfile.add(thefile);
             }
         }
-        this.sendobject(results);
+        this.sendobject(allfile);
     }
-    public void sendobject(Object objeckKirim) throws IOException
+    public String readableFileSize(long size) {
+        if(size <= 0) return "0";
+        final String[] units = new String[] { "B", "kB", "MB", "GB", "TB" };
+        int digitGroups = (int) (Math.log10(size)/Math.log10(1024));
+        return new DecimalFormat("#,##0.#").format(size/Math.pow(1024, digitGroups)) + " " + units[digitGroups];
+    }
+   public void sendobject(Object objeckKirim) throws IOException
     {
         this.objectoutput.writeObject(objeckKirim);
         this.objectoutput.flush();
@@ -109,7 +133,7 @@ public class handler implements Runnable{
                     }
                     
                     fos.write(file.getBytefile());
-                    System.out.println(file.getBytefile());
+                    //System.out.println(file.getBytefile());
                 }
                 
             }
@@ -143,7 +167,7 @@ public class handler implements Runnable{
         file.setNameFile(inputFile.getName());
         file.setArgument(request.getArgument());
         file.setFile(inputFile);
-        byte[] bytefile= new byte[1024];
+        byte[] bytefile= new byte[this.speed];
         int byteread;
         try {
             fis=new FileInputStream(inputFile);
@@ -176,7 +200,7 @@ public class handler implements Runnable{
             {
 
                 System.out.println(request.getCommand() + " " +  request.getArgument());
-                //this.objectinput.reset();
+                
                 if(request.getCommand().equals("LIST") && request.getArgument().equals("USER"))
                 {
                     command = new cmd();
@@ -207,6 +231,12 @@ public class handler implements Runnable{
                     this.createDefFile(request);
                     this.sendobject(command);
                     this.addr=request.getArgument();
+                    this.speed=request.getSpeed();
+                    this.dateFormat=new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                    this.date =new Date();
+                    this.LoginTime=dateFormat.format(date);
+                    if(speed>this.sockcli.getSendBufferSize())this.sockcli.setSendBufferSize(speed);
+                    if(speed>this.sockcli.getReceiveBufferSize())this.sockcli.setReceiveBufferSize(speed);
                 }
                 else if(request.getCommand().equals("LIST") && request.getArgument().equals("FILE"))
                 {
@@ -233,7 +263,11 @@ public class handler implements Runnable{
             Logger.getLogger(handler.class.getName()).log(Level.SEVERE, null, ex);
         }
         System.out.println("connection lost\n");
-        this.alhandler.remove(this);
+        synchronized(this.alhandler)
+        {
+            this.alhandler.remove(this);
+        }
+        
         System.out.println("remove thread\n");
 
         try {            
